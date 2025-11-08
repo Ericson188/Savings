21d03894cd025986c07ee113e7df9cc0d2d6a383
Anuada <ericsonanuada18@gmail.com>
2025-11-09 01:12:51 +0800

    updated now

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,23 @@
-// service-worker.js
-self.addEventListener('install', e => console.log('Service worker installed'));
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('savings-cache-v1').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/manifest.json',
+          '/icons/savings.png',
+          '/icons/savings.png'
+        ]);
+      })
+    );
+    console.log('Service Worker installed ✅');
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+    );
+  });
+  
